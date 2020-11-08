e50fed786df8d376b029fb8cd387afe405d2715c
JoaquimXG <49433786+JoaquimXG@users.noreply.github.com>
2020-11-08 23:19:45 +0000

    Basic styling in place for login modal
    I have added some js to open the modal when clicking the login button
    in header. This is currently in the wrong file, I will move it later.
    
    Modal also currently can't be closed, I am planning to add a small X to
    the top right of the login modal to close it, we should also allow it to
    be closed when clicking anywhere outside of the modal itself.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
 //Insert contents of header tag from header.html file,
 //allows for reuse of header across pages
-$(document).ready(function(){
-    $('#header').load("common/header.html")
-})
+$(document).ready(function() {
+    $("#header").load("common/header.html");
+});
+
+$('#headerLoginButton').click(function(){
+    console.log("test");
+    if ($("#loginModalContainer").css("visibility") == "hidden"){
+        $("#loginModalContainer").css("visibility", "visible");
+    }
+    else{
+        $("#loginModalContainer").css("visibility", "hidden");
+    }
+});
