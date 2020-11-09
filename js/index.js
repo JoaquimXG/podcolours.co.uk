338baf2876ee3dde610cfb96e11f1c597a02ab2c
JoaquimXG <49433786+JoaquimXG@users.noreply.github.com>
2020-11-09 12:10:53 +0000

    Feat(animation for loginModal)

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
     $("#headerLoginButton").click(function() {
         $("#loginModalContainer").css("visibility", "visible");
+        $("#loginModal").css("opacity", "1");
     });
 
     $("#closeLoginModal").click(function() {
+        $("#loginModal").css("opacity", "0");
         $("#loginModalContainer").css("visibility", "hidden");
     });
 
     $('#loginModalContainer').click(function() {
+        $("#loginModal").css("opacity", "0");
         $("#loginModalContainer").css("visibility", "hidden");
     })
 
