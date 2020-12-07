2192929ced00574215dfd052b692bb60265fbbcd
JoaquimXG <49433786+JoaquimXG@users.noreply.github.com>
2020-12-07 14:02:56 +0000

    Tidied up modal handling functionality Added a modal holding div to hold modals that are not currently being displayed and methods for moving modals to and from the holding pen Finalised back button from movie modal

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 import { wordList, resultsText, colourGenreLists } from "./appGlobals.js";
 
+//A collection of functions to manage displaying and closing modals
+import {
+    swapModal,
+    removeModalCloseHandlers,
+    removeModalBackHandlers
+} from "./modalHelpers.js";
+
 //A record of all the cards that the user has decided to keep during the test
 //This Object is also stored in localStorage in the event the browser is closed
 var storedCards = {
@@ -23,49 +30,6 @@ $(function() {
         });
     });
 });
-
-//This function is run every time a card is dropped into either the discard or keep box
-//retrieves the id and color of the card and adds
-//it to the global storedCards object
-function handleCardDrop(_, ui) {
-    var card = $(ui.draggable);
-    var id = card.attr("id");
-    var color = card.attr("data-color");
-
-    if ($(this).attr("id") === "greenDropzone") {
-        storedCards[color].push(id);
-        localStorage.setItem("storedCards", JSON.stringify(storedCards));
-    }
-    card.remove();
-
-    var cards = $(".card");
-    if (cards.length === 0) {
-        if (wordList.length === 0) {
-            calculateResult();
-            return;
-        }
-        displayCards();
-    }
-}
-
-//counts up the results and displays the appropriate modal
-function calculateResult() {
-    var max = 0;
-    var result = "";
-    Object.keys(storedCards).forEach(color => {
-        if (storedCards[color].length > max) {
-            max = color.length;
-            result = color;
-        }
-    });
-
-    //TODO There is a potential case of users discarding every card
-    //A result will not be shown at this point,
-    //We should have a message telling users that they must keep at least 1 card
-    //We can run a check for if (max ==== 0) then they haven't kept any cards
-    localStorage.setItem("resultColor", result);
-    displayResultsModal(result);
-}
 
 //Will add 20 random cards from the deck to the application screen
 function displayCards() {
@@ -97,15 +61,66 @@ function displayCards() {
     });
 }
 
-//Adjusts all css and text for the appropriate results modal
-function displayResultsModal(color) {
+//This function is run every time a card is dropped into either the discard or keep box
+//retrieves the id and color of the card and adds
+//it to the global storedCards object
+function handleCardDrop(_, ui) {
+    var card = $(ui.draggable);
+    var id = card.attr("id");
+    var color = card.attr("data-color");
+
+    if ($(this).attr("id") === "greenDropzone") {
+        storedCards[color].push(id);
+        localStorage.setItem("storedCards", JSON.stringify(storedCards));
+    }
+    card.remove();
+
+    var cards = $(".card");
+    if (cards.length === 0) {
+        if (wordList.length === 0) {
+            calculateResult();
+            return;
+        }
+        displayCards();
+    }
+}
+
+
+//counts up the results and displays the appropriate modal
+function calculateResult() {
+    var max = 0;
+    var result = "";
+    Object.keys(storedCards).forEach(color => {
+        if (storedCards[color].length > max) {
+            max = color.length;
+            result = color;
+        }
+    });
+
+    //TODO There is a potential case of users discarding every card
+    //A result will not be shown at this point,
+    //We should have a message telling users that they must keep at least 1 card
+    //We can run a check for if (max ==== 0) then they haven't kept any cards
+    localStorage.setItem("resultColor", result);
+    generateResultsModal(result);
+    displayResultsModal();
+}
+
+//Generates the required html for results modal
+function generateResultsModal(color) {
     $("#modalTitle").text(resultsText.default);
     $("#modalTitle").append(resultsText[color].color);
     $("#modalTitle").append(resultsText[color].title);
     $("#modalBlurb").text(resultsText[color].blurb);
-    $("#appModalContainer").css("visibility", "visible");
-    $("#appModal").css("opacity", "1");
+}
+
+//Adjusts all css and text for the appropriate results modal
+function displayResultsModal() {
+    swapModal("#resultsModalFormContainer");
+
+    //Removing unneccessary dangling click handlers and icons
     removeModalCloseHandlers();
+    removeModalBackHandlers();
 }
 
 //Calls the OMDB movie API searching for movies with "in the" contained in the title
@@ -113,7 +128,7 @@ function displayResultsModal(color) {
 //Loops through the 10 results and checks if any match a genre for the current color
 //If a genre matches, the loop is broken and the movie is displayed on screen
 function callMovieApi() {
-    function recursiveLoop() {
+    function test10Movies() {
         var yearAfter1980 = Math.round(Math.random() * 40);
 
         var url = `https://www.omdbapi.com/?apikey=d9aa0252&s=in the&y=${1980 +
@@ -135,13 +150,13 @@ function callMovieApi() {
                     continue;
                 }
             }
-            recursiveLoop();
+            test10Movies();
         });
     }
 
     var color = localStorage.getItem("resultColor");
     $("body").addClass("loading");
-    recursiveLoop();
+    test10Movies();
 }
 
 //Checks a movie from a given url for matches agains the current colours genres
@@ -163,47 +178,17 @@ const checkForMatchingGenres = (colorGenres, url) =>
 //Displays the movie modal screen
 //editing modal title, blurb and buttons to suit
 const displayMovieModal = movieData => {
-    console.log(movieData);
-    $("#modalTitle").text("Your results suggests you would like ");
-    $("#modalTitle").append($("<br/>"));
+    //Generate dynamic html from movie data
     var movieTitle = $("<a>")
         .text(movieData.Title)
         .attr("href", `https://www.imdb.com/title/${movieData.imdbID}/`);
-    $("#modalTitle").append(movieTitle);
-    $("#modalBlurb").text(movieData.Plot);
-    var movieButton = $("<button>")
-        .text("More Info")
-        .addClass("buttonBaseline buttonBlue noBorder colCenter");
-    $("#modalButtonContainer").html(movieButton);
+    $("#movieTitle").html(movieTitle);
+    $("#moviePlot").text(movieData.Plot);
+    $("#backAppModal")
+        .css("visibility", "visible")
+        .click(function() {
+            displayResultsModal();
+        });
+
+    swapModal("#movieModalFormContainer");
 };
-
-//Adds all of the required click handlers to allow modals to close
-//also displays the cross
-function addModalCloseHandlers() {
-    //Add click event for close button in the app modal to close the modal
-    $("#closeAppModal").click(function() {
-        handleModalClose();
-    });
-
-    //For the area around the modal
-    $("#appModalContainer").click(function() {
-        handleModalClose();
-    });
-    //Stop click events on the app modal from propogating to is parent and closing the modal
-    $("#appModal").click(function(e) {
-        e.stopPropagation();
-    });
-}
-
-//Removes the click handlers required to be able to close a modal
-//Hides the close button
-function removeModalCloseHandlers(){
-    $("#closeAppModal").off();
-    $("#closeAppModal").css("visibility", "hidden");
-    $("#appModalContainer").off();
-}
-
-function handleModalClose() {
-    $("#appModal").css("opacity", "0");
-    $("#appModalContainer").css("visibility", "hidden");
-}
