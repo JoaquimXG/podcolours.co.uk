98d2541f191a82eeac1510cb46d41cdeafcf37b9
JoaquimXG <49433786+JoaquimXG@users.noreply.github.com>
2021-04-10 16:24:18 +0100

    Logging for logout route

--- routes/logout.js
+++ routes/logout.js
@@ -1,10 +1,14 @@
+const log = require('../logs/logger')
+
 //Sets loggedin value to false if the user was loggedin
 module.exports = (req, res) => {
     if (req.user) {
+        log.info(`Successful logout - User: ${req.user.email}`)
         req.logout()
         res.json({success: true})
     }
     else {
+        log.warn("Bad Logout - User not signed in")
         res.json({success: false})
     }
 }
