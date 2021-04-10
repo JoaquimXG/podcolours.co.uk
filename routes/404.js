526c57249db6a29379ad5aade55deae9423e6b48
JoaquimXG <49433786+JoaquimXG@users.noreply.github.com>
2021-04-10 16:14:46 +0100

    Added logging for 404 and 500 routes

--- routes/404.js
+++ routes/404.js
@@ -1,9 +1,13 @@
+const log = require('../logs/logger')
+
 //Handles wildcard route
 module.exports = (_, res) => {
+    res.status(404)
     var error = {
         title: "Oh no, we seem to be lost...",
         text: "404: Page not Found",
         img: "/img/logo.png",
     };
+    log.warn("404: Page not found", {status: 404})
     res.render("pages/error", { error: error });
 }
--- routes/error.js
+++ routes/error.js
@@ -1,3 +1,5 @@
+const log = require('../logs/logger')
+
 //Error handler, utilised to handle all express error paths
 module.exports = (err, req, res, next) =>{
     var text = err
@@ -7,5 +9,6 @@ module.exports = (err, req, res, next) =>{
         text: text,
         img: "/img/logo.png"
     }
+    log.error(`500: ${text}`, {status: 500})
     res.render("pages/error", {error: error});
 }
