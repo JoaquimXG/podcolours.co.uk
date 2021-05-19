6e15b13bf2081b5209de6b6244c25d2b385ad61b
JoaquimXG <49433786+JoaquimXG@users.noreply.github.com>
2021-05-19 22:42:56 +0100

    Created frontend ajax call to submit email address for password reset

--- public/js/modals/initiatePasswordResetModal.js
+++ public/js/modals/initiatePasswordResetModal.js
@@ -1,10 +1,48 @@
 import {setupModal} from './generalModalHandlers.js'
-
+import toastBuilder from '../components/toast.js'
+    
 function setupInitiatePasswordResetModal(id, openModalButtonId) {
     function setup() {
-        $("#initiateResetButton").click(e => {console.log("TODO Create function to reset password")});
+        $("#initiateResetButton").click(e => {initiatePasswordReset(e)});
     }
     setupModal(id, openModalButtonId, {setup: setup})
+}
+
+function initiatePasswordReset(e) {
+    e.preventDefault()
+
+    const toast = toastBuilder({
+        target: "#initiatePasswordResetModalContainer",
+        defaultText: "A password reset email has been sent"
+    })
+
+    const toastError = toastBuilder({
+        target: "#initiatePasswordResetModalContainer",
+        defaultText: "A server error has occured",
+        classes: "toastError" 
+    })
+
+    var email = $("#initiateResetEmail").val();
+    if (email === ""){
+        $("#initiateResetEmail").addClass("form-field__input--error")
+        return;
+    }
+    $.post({
+        type: "POST",
+        url: "/passwordreset/initiate",
+        data: {email:email},
+        dataType: "json"
+    })
+        .done(() => {
+            toast();
+            setTimeout(() => {
+                window.location.href = "/"
+            }, 2500)
+        })
+        .fail(() => {
+            toastError();
+        })
+
 }
 
 export default setupInitiatePasswordResetModal;
