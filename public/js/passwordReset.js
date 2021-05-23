c861ed6661f0a99c3f4a8dd7f80bad06319b8959
JoaquimXG <49433786+JoaquimXG@users.noreply.github.com>
2021-05-23 21:27:21 +0100

    Adding comments to merge

--- public/js/passwordReset.js
+++ public/js/passwordReset.js
@@ -4,9 +4,14 @@ import {openModal} from './modals/generalModalHandlers.js';
 $(document).ready(function() {
     var id = "resetPasswordModal";
 
+    //Passed close callback returns false to disallow modal from closing
     setupResetPasswordModal(id, "_", {close: () => false});
     $(`#${id}Close`).hide();
+
+    //Remove background and add shadow
     $(`#${id}Container`).css("background", "none")
     $(`#${id}`).css("box-shadow", "rgba(0, 0, 0, 0.35) 0px 2px 8px")
+
+    //Launch page with modal open
     openModal(id)
 });
