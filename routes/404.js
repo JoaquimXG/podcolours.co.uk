4bc870679b2d99ac50a219fa5fe620b02a6c8880
JoaquimXG <49433786+JoaquimXG@users.noreply.github.com>
2021-09-20 10:07:46 +0100

    Hotfix: adding missed page url for 404 log warning

--- routes/404.js
+++ routes/404.js
@@ -1,7 +1,7 @@
 const log = require('../logs/logger')
 
 //Handles wildcard route
-module.exports = (_, res) => {
+module.exports = (req, res) => {
     res.status(404)
     var error = {
         title: "Oh no, we seem to be lost...",
@@ -9,5 +9,5 @@ module.exports = (_, res) => {
         img: "/img/logo.png",
     };
     res.render("pages/error", { error: error });
-    log.warn("404: Page not found", {route: "404", action: "failure", status: 404})
+    log.warn(`404: Page not found: ${req.originalUrl}`, {route: "404", action: "failure", status: 404})
 }
