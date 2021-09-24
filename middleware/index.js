a9f0c09ab7683982b46251984b1da3f963f00366
JoaquimXG <49433786+JoaquimXG@users.noreply.github.com>
2021-09-24 17:44:40 +0100

    Hotfix, formatting

--- middleware/index.js
+++ middleware/index.js
@@ -1,52 +1,57 @@
-require('dotenv').config();
-const log = require('../logs/logger')
+require("dotenv").config();
+const log = require("../logs/logger");
 
-const mongo = require('./mongo')
+const mongo = require("./mongo");
 const express = require("express");
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser');
-const compression = require("compression")
+const cookieParser = require("cookie-parser");
+const compression = require("compression");
 const session = require("express-session");
-const MongoStore = require('connect-mongo')
-const passport = require('./passport')
-const logger = require('./logger')
-const forceHttps = require('./forceHttps')
+const MongoStore = require("connect-mongo");
+const passport = require("./passport");
+const logger = require("./logger");
+const forceHttps = require("./forceHttps");
 
 const app = express();
 
 module.exports = () => {
-    var urlDict = generateMongoUrl()
+    var urlDict = generateMongoUrl();
     log.debug(`=== Connection: ${urlDict.urlWithDatabase} ===`);
     //Include MongoDB as express middleware
-    app.use(mongo(urlDict.urlWithPassword, urlDict.database))
+    app.use(mongo(urlDict.urlWithPassword, urlDict.database));
 
     //Public folder for images, css and js files
     app.use(express.static("public"));
 
     //gzips files before sending responses to server
-    app.use(compression())
+    app.use(compression());
 
     //ejs for templating
     app.set("view engine", "ejs");
 
     //Express sessions for managing user logins
-    var secret = process.env.SESSIONSECRET ? process.env.SESSIONSECRET : "secret"
+    var secret = process.env.SESSIONSECRET
+        ? process.env.SESSIONSECRET
+        : "secret";
     var sess = {
         secret: secret,
         resave: true,
         saveUninitialized: true,
-        cookie: {maxAge: false, },
-        store: MongoStore.create({mongoUrl: urlDict.urlWithPasswordAndDatabase, crypto: {secret: secret}})
-    }
+        cookie: { maxAge: false },
+        store: MongoStore.create({
+            mongoUrl: urlDict.urlWithPasswordAndDatabase,
+            crypto: { secret: secret },
+        }),
+    };
     if (process.env.HTTPS == "true") {
-        app.enable('trust proxy');
+        app.enable("trust proxy");
         sess.cookie.secure = true;
     }
     app.use(session(sess));
 
     //Initialising passport js with sessions
-    app.use(passport.initialize())
-    app.use(passport.session())
+    app.use(passport.initialize());
+    app.use(passport.session());
 
     //HTTP body parse for handling post requests
     app.use(
@@ -57,38 +62,44 @@ module.exports = () => {
 
     //HTTP body parser for json post requests
     app.use(bodyParser.json());
-    app.use(cookieParser())
+    app.use(cookieParser());
 
     //Automatic logging of HTTP requests
-    app.use(logger)
+    app.use(logger);
 
-    //Force HTTPS, a check is contained to only force HTTPS when 
+    //Force HTTPS, a check is contained to only force HTTPS when
     //HTTPS=true environment variable is set
-    app.use(forceHttps)
+    app.use(forceHttps);
     return app;
-}
+};
 
 function generateMongoUrl() {
-    var host = process.env.MONGOHOST ? process.env.MONGOHOST : "localhost"
-    var port = process.env.MONGOPORT ? process.env.MONGOPORT : 27017
-    var database = process.env.MONGODATABASE ? process.env.MONGODATABASE : "podcolours"
+    var host = process.env.MONGOHOST ? process.env.MONGOHOST : "localhost";
+    var port = process.env.MONGOPORT ? process.env.MONGOPORT : 27017;
+    var database = process.env.MONGODATABASE
+        ? process.env.MONGODATABASE
+        : "podcolours";
 
-    var user = process.env.MONGOUSER
-    var password = process.env.MONGOPASSWORD
-    var authSource = process.env.AUTHSOURCE
+    var user = process.env.MONGOUSER;
+    var password = process.env.MONGOPASSWORD;
+    var authSource = process.env.AUTHSOURCE;
 
     if (!user || !password || !authSource) {
-        log.warn("Missing Environment variables for Mongo Authentication - Attempting to connect without authentication")
-        var mongoHostUrl = `mongodb://${host}:${port}/`
-        var urlWithDatabase = `mongodb://${host}:${port}/${database}`
-        var urlWithPassword = mongoHostUrl
-        var urlWithPasswordAndDatabase = urlWithDatabase
+        log.warn(
+            "Missing Environment variables for Mongo Authentication - Attempting to connect without authentication"
+        );
+        var mongoHostUrl = `mongodb://${host}:${port}/`;
+        var urlWithDatabase = `mongodb://${host}:${port}/${database}`;
+        var urlWithPassword = mongoHostUrl;
+        var urlWithPasswordAndDatabase = urlWithDatabase;
     } else {
-        log.debug(`All variables provided for authenticated mongodb connection - Attempting connection with user ${user}`)
-        var mongoHostUrl = `mongodb://${host}:${port}/?authSource=${authSource}`
-        var urlWithDatabase = `mongodb://${host}:${port}/${database}?authSource=${authSource}`
-        var urlWithPassword = `mongodb://${user}:${password}@${host}:${port}/?authSource=${authSource}`
-        var urlWithPasswordAndDatabase = `mongodb://${user}:${password}@${host}:${port}/${database}?authSource=${authSource}`
+        log.debug(
+            `All variables provided for authenticated mongodb connection - Attempting connection with user ${user}`
+        );
+        var mongoHostUrl = `mongodb://${host}:${port}/?authSource=${authSource}`;
+        var urlWithDatabase = `mongodb://${host}:${port}/${database}?authSource=${authSource}`;
+        var urlWithPassword = `mongodb://${user}:${password}@${host}:${port}/?authSource=${authSource}`;
+        var urlWithPasswordAndDatabase = `mongodb://${user}:${password}@${host}:${port}/${database}?authSource=${authSource}`;
     }
 
     return {
@@ -96,6 +107,6 @@ function generateMongoUrl() {
         urlWithDatabase: urlWithDatabase,
         urlWithPasswordAndDatabase: urlWithPasswordAndDatabase,
         host: mongoHostUrl,
-        database: database
-    }
+        database: database,
+    };
 }
